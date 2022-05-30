d1cc263d8d8903c803b2647124a4ab8a3c02ce01
sileneer <yuan95338@gmail.com>
2022-05-31 00:03:59 +0800

    basic function for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(ToDo());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class ToDo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: MainUI());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MainUI extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ToDoState createState() => _ToDoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ToDoState extends State<MainUI> {
+  // save data
+  final List<String> _toDoList = <String>[];
 
-  void _incrementCounter() {
+  // text field
+  final TextEditingController _createTaskController = TextEditingController();
+
+  void initState() {
+    super.initState();
+    getStoredData("todo_storage");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // app layout
+    return Scaffold(
+      appBar: AppBar(title: const Text('To Do')),
+      // body: ListView(children: _getToDoTasks()),
+      body: bodyContent(),
+      // add items to the to-do list
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _popCreateTaskDialog(context),
+          tooltip: 'Add Item',
+          child: Icon(Icons.add)),
+    );
+  }
+
+  bodyContent() {
+    return ListView.builder(
+        itemCount: _toDoList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(_toDoList[index]),
+              onTap: () => _popDeleteTaskDialog(context, index),
+            ),
+          );
+        });
+  }
+
+  // display a dialog for the user to enter items
+  Future<Future> _popCreateTaskDialog(BuildContext context) async {
+    // alter the app state to show a dialog
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Add a task to your list'),
+            content: TextField(
+              controller: _createTaskController,
+              decoration: const InputDecoration(hintText: 'Enter task here'),
+            ),
+            actions: <Widget>[
+              // add button
+              TextButton(
+                child: const Text('ADD'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  _addItem(_createTaskController.text);
+                  SnackBar snackBar = SnackBar(content: Text("Task added"));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+              ),
+              // Cancel button
+              TextButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  // display a dialog for the user to delete items
+  Future _popDeleteTaskDialog(BuildContext context, int index) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext ctx) {
+          return AlertDialog(
+            title: const Text('Delete a task'),
+            content: const Text('Are you sure to delete the task?'),
+            actions: <Widget>[
+              // add button
+              TextButton(
+                child: const Text('DELETE'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _deleteItem(index);
+                  SnackBar snackBar = SnackBar(content: Text("Task deleted"));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+              ),
+              // Cancel button
+              TextButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  void _addItem(String title) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _toDoList.add(title);
+    });
+    writeToStorage(_toDoList);
+    _createTaskController.clear();
+  }
+
+  void _deleteItem(int index) {
+    setState(() {
+      _toDoList.removeAt(index);
+    });
+    writeToStorage(_toDoList);
+  }
+
+  void getStoredData(String key) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      _toDoList.addAll(pref.getStringList(key) ?? List.empty());
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  Future<void> writeToStorage(List<String> s) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('todo_storage', s);
   }
 }
